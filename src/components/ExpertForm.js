23dc09bfc1eeb7e80f72426ea7673352c1dea6d8
afrinajafri <afrinajafri@gmail.com>
2021-06-30 18:26:13 +0800

    added all rules into triggersRules

--- src/components/ExpertForm.js
+++ src/components/ExpertForm.js
@@ -446,7 +446,11 @@ export class ExpertForm extends Component {
     }
 
     triggersRules = () => {
-      return [this._rules_8(), this._rules_9(), this._rules_32()]
+      return [this._rules_8(), this._rules_9(), this._rules_10(), this._rules_11(), this._rules_12(),this._rules_13(), this._rules_14(), this._rules_15(), this._rules_16(), this._rules_17(),
+        this._rules_18(), this._rules_19(), this._rules_20(), this._rules_21(), this._rules_22(),this._rules_23(), this._rules_24(), this._rules_25(), this._rules_26(), this._rules_27(),
+        this._rules_28(), this._rules_29(), this._rules_30(), this._rules_33(), this._rules_32(),this._rules_33(), this._rules_34(), this._rules_35(), this._rules_36(), this._rules_37(),
+        this._rules_38(), this._rules_39(), this._rules_40(), this._rules_41(), this._rules_42(),this._rules_43()
+      ]
       
     }
 
